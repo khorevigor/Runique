955ea131c9474c8654fb3400fd9a2612813e35a3
khorevigor <dostup.96@gmail.com>
2024-06-13 13:58:35 +0200

    Setup SessionStorage

---
+++ core/data/src/main/java/com/dsphoenix/core/data/networking/auth/AuthInfoMapper.kt
@@ -0,0 +1,19 @@
+package com.dsphoenix.core.data.networking.auth
+
+import com.dsphoenix.core.domain.AuthInfo
+
+fun AuthInfo.toSerializable(): AuthInfoSerializable {
+    return AuthInfoSerializable(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
+
+fun AuthInfoSerializable.toAuthInfo(): AuthInfo {
+    return AuthInfo(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
--- core/data/src/main/java/com/dsphoenix/core/data/networking/di/CoreDataModule.kt
+++ core/data/src/main/java/com/dsphoenix/core/data/networking/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.dsphoenix.core.data.networking.di
 
 import com.dsphoenix.core.data.networking.HttpClientFactory
+import com.dsphoenix.core.data.networking.auth.EncryptedSessionStorage
+import com.dsphoenix.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
