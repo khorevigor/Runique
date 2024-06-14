92f3a6d58666cca95525a6a61ae13d9cc80ada32
khorevigor <dostup.96@gmail.com>
2024-06-14 16:44:23 +0200

    Add splash screen and keep user logged in

--- app/src/main/java/com/dsphoenix/runique/MainActivity.kt
+++ app/src/main/java/com/dsphoenix/runique/MainActivity.kt
@@ -3,16 +3,33 @@ package com.dsphoenix.runique
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.dsphoenix.core.presentation.designsystem.RuniqueTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModel<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.state.isCheckingAuth
+            }
+        }
+
         setContent {
             RuniqueTheme {
-                val navController = rememberNavController()
-                NavigationRoot(navController = navController)
+                if (!viewModel.state.isCheckingAuth) {
+                    val navController = rememberNavController()
+                    NavigationRoot(
+                        navController = navController,
+                        isLoggedIn = viewModel.state.isLoggedIn
+                    )
+
+                }
             }
         }
     }
---
+++ app/src/main/java/com/dsphoenix/runique/MainState.kt
@@ -0,0 +1,6 @@
+package com.dsphoenix.runique
+
+data class MainState(
+    val isLoggedIn: Boolean = false,
+    val isCheckingAuth: Boolean = false
+)
