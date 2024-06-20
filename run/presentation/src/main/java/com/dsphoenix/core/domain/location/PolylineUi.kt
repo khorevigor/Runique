c772ea8fb3d9aec81df49742fb8985fb4f2990a1
khorevigor <dostup.96@gmail.com>
2024-06-20 22:58:17 +0200

    Draw tracked routed by polylines

---
+++ run/presentation/src/main/java/com/dsphoenix/core/domain/location/PolylineUi.kt
@@ -0,0 +1,9 @@
+package com.dsphoenix.core.domain.location
+
+import androidx.compose.ui.graphics.Color
+
+data class PolylineUi(
+    val location1: Location,
+    val location2: Location,
+    val color: Color
+)
