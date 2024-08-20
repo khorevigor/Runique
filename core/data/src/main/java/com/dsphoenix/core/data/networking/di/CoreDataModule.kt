bfb09c85aefd5e6c5011743c9cf1fb0eda011da6
khorevigor <dostup.96@gmail.com>
2024-08-20 22:53:26 +0200

    Delete no more used HttpClient-related code

--- core/data/src/main/java/com/dsphoenix/core/data/networking/di/CoreDataModule.kt
+++ core/data/src/main/java/com/dsphoenix/core/data/networking/di/CoreDataModule.kt
@@ -1,6 +1,5 @@
 package com.dsphoenix.core.data.networking.di
 
-import com.dsphoenix.core.data.networking.HttpClientFactory
 import com.dsphoenix.core.data.networking.auth.EncryptedSessionStorage
 import com.dsphoenix.core.data.networking.run.ConnectivityChecker
 import com.dsphoenix.core.data.networking.run.OfflineFirstRunRepository
@@ -11,9 +10,6 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
-    single {
-        HttpClientFactory(get()).build()
-    }
     singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
     singleOf(::OfflineFirstRunRepository).bind<RunRepository>()
     singleOf(::ConnectivityChecker)
